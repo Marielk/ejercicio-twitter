dd6bc6a78ccc175d661566b9e86667a863db2317
Marielk <marielk.mya@gmail.com>
2018-06-12 14:00:19 -0300

    intento de v2 sin exito aun

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
+/*
+let camposValidos = document.getElementById('userComment').value;
+const botonTweet = document.getElementById('tweetButton');
+if (camposValidos === 0 || camposValidos === null) {
+  botontweet.disabled = true;
+}
+else {
+  botonTweet.disabled = false;
+} */
+
 const button = document.getElementById('tweetButton');
+button.disabled = true;
+let userText = document.getElementById('userComment').value;
+if (userText.length != 0 || userText != null) {
+  button.disabled = false;
+} else { button.disabled = true; }
+
 button.addEventListener('click', () => {
   let userText = document.getElementById('userComment').value;
   document.getElementById('userComment').value = ' '; 
@@ -7,16 +23,20 @@ button.addEventListener('click', () => {
 
   const newComments = document.createElement('div');
 
-  if (userText.length === 0 || userText === null) {
-    alert('Debes ingresar un mensaje');
-    return false; 
-  }
-
   let newTextComments = document.createTextNode(userText);
-
+  
   const contenedorElemento = document.createElement('p');
   contenedorElemento.appendChild(newTextComments);
   newComments.appendChild(contenedorElemento);
-  showCommentDiv.appendChild(newComments);
+  showCommentDiv.appendChild(newComments); 
 
 })
+/*
+boton.disabled = false;
+    else boton.disabled = true;
+    
+    if (userText.length === 0 || userText === null) {
+      alert('Debes ingresar un mensaje');
+      return false; 
+    } */ 
+  
